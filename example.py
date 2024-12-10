cbeda8e014bec4acb3be91c164d8f4ea78ea775a
Kardelen Yurtkuran <101009678+kardelenyurtkuran@users.noreply.github.com>
2024-12-10 14:55:40 +0300

    update

--- example.py
+++ example.py
@@ -1,23 +1,40 @@
+import seaborn as sns
+import matplotlib.pyplot as plt
 import pandas as pd
 
-# Veri setini yükleyin
 df = pd.read_csv("heart_statlog_cleveland_hungary_final.csv")
+# Cinsiyet ve Kolesterol ilişkisini görselleştir
+# plt.figure(figsize=(8, 6))
+# sns.boxplot(x='sex', y='cholesterol', data=df)
+# plt.title('Cinsiyet ve Kolesterol İlişkisi')
+# plt.xlabel('Cinsiyet (0: Kadın, 1: Erkek)')
+# plt.ylabel('Kolesterol (mg/dL)')
+# plt.show()
 
-# Aykırı değerlerin işleneceği sütunlar ve koşulları
-columns_with_outliers = {
-    'cholesterol': 0,          # 0 anlam dışı
-    'resting bp s': 0,         # 0 anlam dışı
-    'oldpeak': lambda x: x < 0  # Negatif değerler anlam dışı
-}
+# Yaş ve Kolesterol ilişkisini görselleştir
+# plt.figure(figsize=(8, 6))
+# sns.scatterplot(x='age', y='cholesterol', data=df)
+# plt.title('Yaş ve Kolesterol İlişkisi')
+# plt.xlabel('Yaş')
+# plt.ylabel('Kolesterol (mg/dL)')
+# plt.show()
 
-# Her bir sütun için aykırı değerleri medyan ile dolduralım
-for column, condition in columns_with_outliers.items():
-    if callable(condition):  # Koşul bir fonksiyon ise (örneğin negatif değer kontrolü)
-        median_value = df.loc[~df[column].apply(condition), column].median()
-        df.loc[df[column].apply(condition), column] = median_value
-    else:  # Koşul sabit bir değer ise (örneğin 0 kontrolü)
-        median_value = df.loc[df[column] != condition, column].median()
-        df.loc[df[column] == condition, column] = median_value
+# Cinsiyet ve Kalp Hastalığı ilişkisini görselleştir
+# plt.figure(figsize=(8, 6))
+# sns.countplot(x='sex', hue='target', data=df)
+# plt.title('Cinsiyet ve Kalp Hastalığı İlişkisi')
+# plt.xlabel('Cinsiyet (0: Kadın, 1: Erkek)')
+# plt.ylabel('Hastaların Sayısı')
+# plt.legend(title='Kalp Hastalığı', labels=['Yok', 'Var'])
+# plt.show()
 
-# Güncel veri setinin istatistiklerini inceleyin
-print(df.describe())
+# Yaş ve Kalp Hastalığı ilişkisini görselleştir
+plt.figure(figsize=(8, 6))
+sns.boxplot(x='target', y='age', data=df)
+plt.title('Yaş ve Kalp Hastalığı İlişkisi')
+plt.xlabel('Kalp Hastalığı Durumu (0: Yok, 1: Var)')
+plt.ylabel('Yaş')
+plt.show()
+
+
+
